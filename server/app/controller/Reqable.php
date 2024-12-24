13c5862de641d652d0b5c9e1f710901bebfa412c
chenting <2350906567@qq.com>
2024-12-25 00:29:46 +0800

    gpt流程已跑通

--- server/app/controller/Reqable.php
+++ server/app/controller/Reqable.php
@@ -14,63 +14,63 @@ class Reqable extends BaseController
     {
         $data = $request->param();
 
+        //将变量data转成json格式，才能存储在txt文件里,调试使用
+//        $data_str = json_encode($data, JSON_UNESCAPED_UNICODE);
+//        $data_str = $data_str . ",\n";
+//        file_put_contents('data.txt', $data_str, FILE_APPEND);
+//        $data_str = Db::name('http_logs')->select();
+
         $data = $data['log']['entries'];
+//        var_dump($data);exit;
         foreach ($data as $key => $value) {
+//            var_dump($value);exit;
             $request_time = $value['startedDateTime'];
             $arr['source_ip'] = $value['serverIPAddress'];
             $arr['target_domain'] = $value['request']['url'];
             $arr['request_type'] = $value['request']['method'];
             $arr['status_code'] = $value['response']['status'];
+//            $arr['bodySize'] = $value['response']['content']['size'];
 
-            // 将请求和响应数据转换为JSON格式
+            // 将请求和响应数据转换为JSON格式的字符串
             $arr['request'] = json_encode($value['request'], JSON_UNESCAPED_UNICODE);
             $arr['response'] = json_encode($value['response'], JSON_UNESCAPED_UNICODE);
-
-            $arr['bodySize'] = $value['response']['bodySize'];
-
+//            var_dump($arr['response'] );exit;
+//            $arr['bodySize'] = $value['response']['bodySize'];
             $dateTime = new DateTime($request_time);
             //设置时区为当前时区（如果需要的话，你可以根据实际情况调整时区）
             $dateTime->setTimezone(new \DateTimeZone(date_default_timezone_get()));
             $arr['request_time'] = $dateTime->format('Y-m-d H:i:s');
-
             DB::name('http_logs')->insert($arr);
-
+            reqable::test();
         }
         // 返回成功响应
         return json(['status' => 'success', 'message' => 'Data saved to file']);
     }
 
-    public
-    function test()
+    public static function test()
     {
-        $data = file_get_contents('../data.json');
-        $data = json_decode($data, true);
+        $data_str = Db::name('http_logs')->select();
+        foreach ($data_str as $k0 => $v0) {
+//            var_dump($v0['response']['content']['text']);
+            //将变量$v0的值（字符串）转换成数组
+            $data_arr = json_decode($v0['response'], TRUE);
+//            exit;
+//            var_dump($size);
 
+            //将数组转换成字符串,插入数据库
+            $data_str['headers'] = json_encode($data_arr['headers']);
+            $data_str['status'] = json_encode($data_arr['status']);
+            //取数组的值，取出来的数据类型是字符串
+            $size_str = $data_arr['content']['size'];
+            $body_str = $data_arr['content']['text'];
 
-//        var_dump($data);
-//        $data = $data['log']['entries'];
-//        var_dump($data);
-        foreach ($data as $key => $value) {
-            $request_time = $value['startedDateTime'];
-            $arr['source_ip'] = $value['serverIPAddress'];
-            $arr['target_domain'] = $value['request']['url'];
-            $arr['request_type'] = $value['request']['method'];
-            $arr['status_code'] = $value['response']['status'];
-
-            // 将请求和响应数据转换为JSON格式
-            $arr['request'] = json_encode($value['request'], JSON_UNESCAPED_UNICODE);
-            $arr['response'] = json_encode($value['response'], JSON_UNESCAPED_UNICODE);
-
-            $arr['bodySize'] = $value['response']['bodySize'];
-
-            $dateTime = new DateTime($request_time);
-            //设置时区为当前时区（如果需要的话，你可以根据实际情况调整时区）
-            $dateTime->setTimezone(new \DateTimeZone(date_default_timezone_get()));
-            $arr['request_time'] = $dateTime->format('Y-m-d H:i:s');
-
-            DB::name('http_logs')->insert($arr);
-
+            //插入响应头、响应体、状态码、bodySize
+            DB::name('http_logs')->where('id', $v0['id'])->update([
+                'response_headers' => $data_str['headers'],
+                'response_body' => $body_str,
+                'status_code' => $data_str['status'],
+                'bodySize' => $size_str,
+            ]);;
         }
     }
-
 }
--- server/app/model/aimodel.php
+++ server/app/model/aimodel.php
@@ -5,6 +5,7 @@ namespace app\model;
 
 use think\facade\Db;
 use think\Model;
+use think\tests\ValidateTest;
 
 /**
  * @mixin \think\Model
@@ -14,20 +15,29 @@ class aimodel extends Model
     //
     public static function aimodel()
     {
-//        $list = Db::name('http_logs')->whereNull('ai_result')->limit(100)->select()->toArray();
-        $list = Db::name('http_logs')->where('id', 246)->limit(1)->select()->toArray();
-//        var_dump($list);
-//        exit;
+        //搜索response_body中包含html的内容
+        $list = Db::name('http_logs')->whereLike('response_body', '%<html%')->limit(3)->select()->toArray();
+//        var_dump($list);exit;
+
         foreach ($list as $k => $v) {
-//            $arr = json_decode($v['request'], true);
-//            $arr = json_encode($v['response']);
-            $arr = $v['response'];
-            var_dump($arr);
+            $arr_headers = $v['response_headers'];
+            $arr_body = $v['response_body'];
+
+            //将响应头信息转换成数组，键与值对应 method：POST
+            $headers_str = '';
+            $arr_headers = json_decode($arr_headers, true);
+            foreach ($arr_headers as $item) {
+                $key = ltrim($item['name'], ':');
+                //使用.= 将键值的内容保存到headers_str中，并且换行
+                $headers_str .= $key . "：" . $item['value'] . "\n";
+
+            }
+//            var_dump($headers_str);
 //            exit;
+
             $messages = [
                 ["role" => "system", "content" => "You are a helpful assistant."],
-                ["role" => "user", "content" => '帮我识别这个网站用到了那些应用,例如(Jquery),并返回每一个应用指纹的规则' . $arr . '返回格式参考:<<<
-
+                ["role" => "user", "content" => '帮我识别这个网站用到了那些应用,例如(Jquery),并返回每一个应用指纹的规则,响应头信息：' . $headers_str . '\n' . '响应体信息：' . $arr_body . '返回格式参考:<<<
 [
 
 {
@@ -51,23 +61,33 @@ class aimodel extends Model
 ]
 >>>']
             ];
-
+            var_dump($messages);
             //调用大模型接口
-//            $result_string = sendMessage($messages);
-//            //将$result的内容保存到result.txt文件里
-//            $txt = file_put_contents('result.txt', $result_string);
+            $result_string = sendMessage($messages);
+//            var_dump($result_string);exit;
 
-            //从result.txt里读取数据
-            $result = file_get_contents('result.txt');
-            $result_arr = json_decode($result, true);
-//            var_dump($result_arr);exit;
+//            // 将每行文件用,结尾，并且换行
+//            $txt = $result_string . ",\n";
+//            //将$result的内容保存到result.txt文件里
+//            file_put_contents('result.json', $txt, FILE_APPEND);
+//            //从result.txt里读取数据
+//            $result = file_get_contents('result.json');
+//            var_dump($result);
+//            exit;
+//            $result_arr = json_decode($result, true);
+
+            $result_arr = json_decode($result_string, true);
+//            $result_arr = json_decode($result_string['choices']['message']['content'], true);
+            var_dump($result_arr['choices'][0]['message']['content']);
+//            exit;
+//
             //过滤字符串
             $result_str = aimodel::extractJsonData($result_arr['choices'][0]['message']['content']);
 
             $result_arr = json_decode($result_str, true);
             $error = json_last_error_msg();
             var_dump($error);
-//
+
             //判断$result是否为空，如果为空则跳过当前循环
             if (empty($result_arr)) {
                 continue;
@@ -75,7 +95,7 @@ class aimodel extends Model
 
             //将$result二维数组 的结果插入到fingers数据表中
             foreach ($result_arr as $kk => $vv) {
-                var_dump($v['target_domain']);
+//                var_dump($v['target_domain']);
 //                var_dump($vv);exit;
                 $data = [
                     'finger_id' => $v['id'],
